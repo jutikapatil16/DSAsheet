7cea4dfbfe59d8520894b39de50e6392c43cce57
Jutika Patil <jutikapatil123@gmail.com>
2023-01-06 17:21:42 +0530

    first_commit

---
+++ 16array.java
@@ -0,0 +1,22 @@
+
+class 16array{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        long min_diff = Integer.MAX_VALUE;
+        
+        Collections.sort(a);
+        
+        //optional
+        if(n ==0 || m ==0){
+                return 0;
+            }
+            //
+        for(int i =0 ; i + m - 1 < n ; i++){
+            
+            if(a.get(i+m-1) - a.get(i) < min_diff){
+             min_diff = a.get(i+m-1) - a.get(i);
+            }
+        }
+        return min_diff; 
+    }
+}
