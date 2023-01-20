dcc2ba1951ecceee4bca033970528aedd36cd738
Jutika Patil <jutikapatil123@gmail.com>
2023-01-21 00:00:21 +0530

    22 code

--- 54array.java
+++ 54array.java
@@ -1,5 +1,8 @@
 class GFG {
-
+  // For each character, store the sequence which should be obtained at its respective position in an array, i.e. for Z, store 9999. For Y, store 999. For K, store 55 and so on.
+  // For each character, subtract ASCII value of ‘A’ and obtain the position in the array pointed  by that character and add the sequence stored in that array to a string.
+  // If the character is a space, store 0
+  // Print 
   // Function which computes the sequence
   static String printSequence(String arr[], String input)
   {
