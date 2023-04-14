016c753e61268403b37b25cf04a07a6d876f573c
Jutika Patil <jutikapatil123@gmail.com>
2023-04-14 17:14:06 +0530

    code44

--- 78array2D.java
+++ 78array2D.java
@@ -1,73 +1,26 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while(t-- > 0)
-        {
-            String S[] = read.readLine().split(" ");
-            int R = Integer.parseInt(S[0]);
-            int C = Integer.parseInt(S[1]);
-            int matrix[][] = new int[R][C];
-            int c = 0;
-            for(int i = 0; i < R; i++){
-                String line[]=read.readLine().trim().split(" ");
-                for(int j = 0; j < C; j++){
-                    matrix[i][j] = Integer.parseInt(line[j]);
-                }
-            }
-            Solution ob = new Solution();
-            int ans = ob.median(matrix, R, C);
-            System.out.println(ans);
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-//User function Template for Java
-
 class Solution {
-    int median(int matrix[][], int R, int C) {
+  public void setZeroes(int[][] matrix) {
 
-//         // brute force
 
-        ArrayList<Integer> al = new ArrayList<>();
-        
-        for(int i=0;i<R;i++){
-            for(int j=0;j<C;j++){
-                al.add(matrix[i][j]);
-                
-            }
-        }
-        
-        Collections.sort(al);
-      int out=0;    
-      int m = R*C;
-       
-      int i = 0 , j = m-1;
-      while(i < m && j >=0){
-           
-            if(i==j){
-              out = al.get(i);
-              break;
-            }
-            
-            j--; i++;
-        }
-	    
-// 	  int out = al.size()/2;
-	    
-      return out;  
+// basically using array to store the row number and column number and using a flag ( here -1 ) 
+  int[] row = new int[matrix.length];
+      int[] col = new int[matrix[0].length];
 
-}
+      for (int i = 0; i < matrix.length; i++) {
+          for (int j = 0; j < matrix[0].length; j++) {
+              if (matrix[i][j] == 0) {
+                  row[i] = -1;
+                  col[j] = -1;
+              }
+          }
+      }
+
+      for (int i = 0; i < matrix.length; i++) {
+          for (int j = 0; j < matrix[0].length; j++) {
+              if (row[i] == -1 || col[j] == -1) {
+                  matrix[i][j] = 0;
+              }
+          }
+      }
+  }
 }
